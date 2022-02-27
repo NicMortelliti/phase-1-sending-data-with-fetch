f3777b8847517481c1b6e54d16c8c80b3385fad8
NicMortelliti <npmortelliti@gmail.com>
2022-02-26 17:11:36 -0800

    commit tree 671b4017225f695929bc1b3f2342433a3dbeaa7c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+/*
+Function submitData takes:
+users name (string)
+users email (string)
+*/
+function submitData(userName, userEmail) {
+  // Assemble data from form submission
+  const formData = {
+    name: userName,
+    email: userEmail,
+  };
+
+  /*
+  Create the configurationObject that
+  will be passed in to the fetch
+  */
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return (
+    fetch("http://localhost:3000/users", configurationObject)
+      .then(res => res.json())
+      // Add object ID to DOM
+      .then(obj => {
+        document.querySelector("body").innerHTML = obj.id;
+      })
+      .catch(function (error) {
+        document.querySelector("body").innerHTML = error.message;
+      })
+  );
+}
